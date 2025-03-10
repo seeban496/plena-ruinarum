223f08b202f0b7a863a9631f3e24ac99208f01d6
si_hyun <seeban1712@gmail.com>
2025-03-11 03:25:52 +0900

    Update page.tsx

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -4,7 +4,7 @@ import { config } from "@/config";
 import { signOgImageUrl } from "@/lib/og-image";
 import Markdown from "react-markdown";
 
-const content = `# 플랫폼에 대한 고민
+const content = `# Hi / 您好 / Здравствуйте / Χαίρετε
 
 Adult, R1999 주력, Her or It
 
@@ -14,9 +14,15 @@ nomen: 라틴어, '이름'
 
 occultum: occultus의 형용사, '미지의'
 
-줄이면 익명이에요.<br><br>안 바쁘면 립9를 하며, FUB FREE
+줄이면 익명이에요.
 
-무례하다는 게 사람마다 기준이 다를 겁니다. 편하게 차단해주세요, 감사합니다.`;
+안 바쁘면 립9를 하고 있어요.
+
+집과 연고자라고 할 만한 사람이 없어 자주 떠돌아다니며, 돈이 필요하면 벌어다가 씁니다. 여행은 아니에요. 본업은 대한민국에서 금융(M&A, Audit)을 주력으로 맡았었고, 지금은 병이 들어 치료받고 있어요.
+
+무례하다는 게 사람마다 기준이 다를 겁니다. 편하게 차단해주세요, 감사합니다.
+
+동시에, 세심한 부분까지 헤아리지 못해 늘 미안합니다.`;
 
 export async function generateMetadata() {
   return {
