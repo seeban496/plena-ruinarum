7d837983911e0455ee54622c726485e769f16795
si_hyun <seeban1712@gmail.com>
2025-10-16 10:08:06 +0800

    Update NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export const NotFound = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold">404</h1>
+      <h1 className="text-5xl font-bold">Sorry...404</h1>
       <p className="mt-2 text-2xl">Page not found</p>
 
       <Button asChild className="mt-4">
